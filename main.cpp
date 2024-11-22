14fcf9546ca3f618c52b96685d3e8d491b316a12
tetsuji yamamoto <241325@st.yoshida-g.ac.jp>
2024-11-23 02:39:00 +0900

    a

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include "title.h"
 #include "game.h"
 #include "result.h"
+#include "guide.h"
 
 //グローバル変数宣言
 LPDIRECT3D9 g_pD3D = NULL;						//Direct3Dオブジェクトへのポインタ
@@ -27,129 +28,131 @@ bool g_isFullscreen = false;					//ウィンドウを切り替えるためのフラグ
 RECT g_windowRect;								//ウィンドウを切り替えるための変数
 int g_line = 0;
 
-//=======================
-//デバイスの取得
-//=======================
+//***********************************************
+// デバイスの取得
+//***********************************************
 LPDIRECT3DDEVICE9 GetDevice(void)
 {
 	return g_pD3DDevice;
 }
 
-//=======================
-//メイン関数
-//=======================
+//***********************************************
+// メイン関数
+//***********************************************
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstancePrev,_In_ LPSTR lpCmdLine,_In_ int nCmdShow)
 {
-	WNDCLASSEX wcex =					//ウィンドウクラスの構造体
-	{
-		sizeof(WNDCLASSEX),				//WNDCLASSEXのメモリサイズ
-		CS_CLASSDC,						//ウィンドウのスタイル
-		WindowProc,						//ウィンドウプロシージャ
-		0,								//0にする(通常は使用しない)
-		0,								//0にする(通常は使用しない)	
-		hInstance,						//インスタンスハンドル
-		LoadIcon(NULL,IDI_APPLICATION),	//タスクバーのアイコン
-		LoadCursor(NULL,IDC_ARROW),		//マウスカーソル
-		(HBRUSH)(COLOR_WINDOW + 1),		//クライアント領域の背景色
-		NULL,							//メニューバー
-		CLASS_NAME,						//ウィンドウクラスの名前
-		LoadIcon(NULL,IDI_APPLICATION)	//ファイルのアイコン
+	WNDCLASSEX wcex =					// ウィンドウクラスの構造体
+	{									   
+		sizeof(WNDCLASSEX),				// WNDCLASSEXのメモリサイズ
+		CS_CLASSDC,						// ウィンドウのスタイル
+		WindowProc,						// ウィンドウプロシージャ
+		0,								// 0にする(通常は使用しない)
+		0,								// 0にする(通常は使用しない)	
+		hInstance,						// インスタンスハンドル
+		LoadIcon(NULL,IDI_APPLICATION),	// タスクバーのアイコン
+		LoadCursor(NULL,IDC_ARROW),		// マウスカーソル
+		(HBRUSH)(COLOR_WINDOW + 1),		// クライアント領域の背景色
+		NULL,							// メニューバー
+		CLASS_NAME,						// ウィンドウクラスの名前
+		LoadIcon(NULL,IDI_APPLICATION)	// ファイルのアイコン
 	};
 
-	RECT rect = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };//画面サイズの構造体
+	RECT rect = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };	// 画面サイズの構造体
 		
 	HWND hWnd;
 	MSG msg;
-	DWORD dwCurrentTime;//現在時刻
-	DWORD dwExecLastTime;//現在時刻
-	DWORD dwFrameCount;//フレームカウント
-	DWORD dwFPSLastTime;//最後にFPSを計測した時刻
+	DWORD dwCurrentTime;	// 現在時刻
+	DWORD dwExecLastTime;	// 現在時刻
+	DWORD dwFrameCount;		// フレームカウント
+	DWORD dwFPSLastTime;	// 最後にFPSを計測した時刻
 
-	//ウィンドクラスの登録
+	// ウィンドクラスの登録
 	RegisterClassEx(&wcex);
 
-	//クライアント領域を指定のサイズに調整
+	// クライアント領域を指定のサイズに調整
 	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
 
 	//ウィンドウを生成
-	hWnd = CreateWindowEx(0,		//拡張ウィンドウスタイル
-		CLASS_NAME,					//ウィンドウクラスの名前
-		WINDOW_NAME,				//ウィンドウの名前
-		WS_OVERLAPPEDWINDOW,		//ウィンドウスタイル
-		CW_USEDEFAULT,				//ウィンドウ左上X座標
-		CW_USEDEFAULT,				//ウィンドウ左上Y座標
-		(rect.right - rect.left),	//ウィンドウの幅
-		(rect.bottom - rect.top),	//ウィンドウの高さ
-		NULL,						//親ウィンドウのハンドル
-		NULL,						//メニューハンドルまたは子ウィンドウのハンドル
-		hInstance,					//インスタンスハンドル
-		NULL);						//ウィンドウ生成データ
+	hWnd = CreateWindowEx(0,		// 拡張ウィンドウスタイル
+		CLASS_NAME,					// ウィンドウクラスの名前
+		WINDOW_NAME,				// ウィンドウの名前
+		WS_OVERLAPPEDWINDOW,		// ウィンドウスタイル
+		CW_USEDEFAULT,				// ウィンドウ左上X座標
+		CW_USEDEFAULT,				// ウィンドウ左上Y座標
+		(rect.right - rect.left),	// ウィンドウの幅
+		(rect.bottom - rect.top),	// ウィンドウの高さ
+		NULL,						// 親ウィンドウのハンドル
+		NULL,						// メニューハンドルまたは子ウィンドウのハンドル
+		hInstance,					// インスタンスハンドル
+		NULL);						// ウィンドウ生成データ
 
-	//初期化処理
+	// 初期化処理
 	if (FAILED(Init(hInstance, hWnd, TRUE)))
-	{//初期化処理が失敗したとき
+	{// 初期化処理が失敗したとき
 		return -1;
 	}
 
-	//分解能を設定
+	// 分解能を設定
 	timeBeginPeriod(1);
 	dwCurrentTime = 0;
 	dwExecLastTime = timeGetTime();
 
-	//ウィンドウの表示
-	ShowWindow(hWnd, nCmdShow);	//ウィンドウの表示状態を設定
-	UpdateWindow(hWnd);			//クライアント領域を更新
+	// ウィンドウの表示
+	ShowWindow(hWnd, nCmdShow);	// ウィンドウの表示状態を設定
+	UpdateWindow(hWnd);			// クライアント領域を更新
 	
 	dwFrameCount = 0;
 	dwFPSLastTime = timeGetTime();
 
-	//メッセージループ
+	// メッセージループ
 	while (1)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0)
-		{//windoesの処理
+		{// windoesの処理
 			if (msg.message == WM_QUIT)
-			{//WM_QUITメッセージを受け取ったらメッセージループを抜ける
+			{// WM_QUITメッセージを受け取ったらメッセージループを抜ける
 				break;
 			}
 			else
 			{
-				//メッセージの設定
-				TranslateMessage(&msg);	//仮想メッセージを文字メッセージへ変換
-				DispatchMessage(&msg);	//ウィンドウプロシージャへメッセージを送出
+				// メッセージの設定
+				TranslateMessage(&msg);	// 仮想メッセージを文字メッセージへ変換
+				DispatchMessage(&msg);	// ウィンドウプロシージャへメッセージを送出
 			}
 		}
 		else
-		{//DilectXの処理
-			dwCurrentTime = timeGetTime();//現在時刻を取得
+		{// DilectXの処理
+			dwCurrentTime = timeGetTime();	// 現在時刻を取得
 			if ((dwCurrentTime - dwFPSLastTime) >= 500)
-			{//0.5秒経過
-				//FPSを計測
+			{// 0.5秒経過
+				// FPSを計測
 				g_nCountFPS = (dwFrameCount * 1000) / (dwCurrentTime - dwFPSLastTime);
-				dwFPSLastTime = dwCurrentTime;											//FPSを測定したを保存
-				dwFrameCount = 0;														//フレームカウントをクリア
+				dwFPSLastTime = dwCurrentTime;											// FPSを測定したを保存
+				dwFrameCount = 0;														// フレームカウントをクリア
 			}
 
-			if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60))//60フレーム
+			if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60))	// 60フレーム
 			{
 				dwExecLastTime = dwCurrentTime;
-				//更新処理
+				// 更新処理
 				Updata();
-				//描画処理
+
+				// 描画処理
 				Draw();
-				dwFrameCount++;//フレームカウントを加算
+
+				dwFrameCount++;	// フレームカウントを加算
 			}
 		}
 	}
-	//ウィンドウクラスの登録を解除
+	// ウィンドウクラスの登録を解除
 	UnregisterClass(CLASS_NAME, wcex.hInstance);
 
 	return (int)msg.wParam;
 }
 
-//==========================================
+//***********************************************
 // ウィンドウをフルスクリーンに変える処理
-//==========================================
+//***********************************************
 void ToggleFullscreen(HWND hWnd)
 {
 	// 現在のウィンドウスタイルを取得
@@ -177,25 +180,25 @@ void ToggleFullscreen(HWND hWnd)
 	g_isFullscreen = !g_isFullscreen;
 }
 
-//==========================================
+//***********************************************
 //ウィンドプロシージャ
-//==========================================
+//***********************************************
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_DESTROY://ウィンド破棄メッセージ
+	case WM_DESTROY:	// ウィンド破棄メッセージ
 
-		//WM_QUITメッセージを送る
+		// WM_QUITメッセージを送る
 		PostQuitMessage(0);
 		break;
 
-	case WM_KEYDOWN://キー押下のメッセージ
+	case WM_KEYDOWN:	// キー押下のメッセージ
 		switch (wParam)
 		{
-		case VK_ESCAPE:	//ESCが押されたら
+		case VK_ESCAPE:	// ESCが押されたら
 
-			//ウィンドウを破棄する(EM_DESTROYメッセージを送る)
+			// ウィンドウを破棄する(EM_DESTROYメッセージを送る)
 			DestroyWindow(hWnd);
 			break;
 
@@ -209,39 +212,39 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
-//==========================================
-//初期化しょり
-//==========================================
+//***********************************************
+// 初期化しょり
+//***********************************************
 HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 {
-	D3DDISPLAYMODE d3ddm;//ぢスプレイモード
-	D3DPRESENT_PARAMETERS d3dpp;//プレゼンテーションパラメータ
+	D3DDISPLAYMODE d3ddm;			// ディスプレイモード
+	D3DPRESENT_PARAMETERS d3dpp;	//プレゼンテーションパラメータ
 
-	//Direc3Dオブジェクトの生成
+	// Direc3Dオブジェクトの生成
 	g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
 	if (g_pD3D == NULL)
 	{
 		return E_FAIL;
 	}
-	//現在のディスプレイモードを取得
+	// 現在のディスプレイモードを取得
 	if (FAILED(g_pD3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &d3ddm)))
 	{
 		return E_FAIL;
 	}
-	//デバイスのプレゼンテーションパラメータを設定
-	ZeroMemory(&d3dpp, sizeof(d3dpp));//パラメータのゼロクリア
-	d3dpp.BackBufferWidth = SCREEN_WIDTH;//ゲーム画面サイズ幅
-	d3dpp.BackBufferHeight = SCREEN_HEIGHT;//ゲーム画面サイズ高さ
-	d3dpp.BackBufferFormat = d3ddm.Format;//バックバッファの形式
-	d3dpp.BackBufferCount = 1;//
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;//
-	d3dpp.EnableAutoDepthStencil = TRUE;//
-	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;//
-	d3dpp.Windowed = bWindow;//
-	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;//
-	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;//
+	// デバイスのプレゼンテーションパラメータを設定
+	ZeroMemory(&d3dpp, sizeof(d3dpp));			// パラメータのゼロクリア
+	d3dpp.BackBufferWidth = SCREEN_WIDTH;		// ゲーム画面サイズ幅
+	d3dpp.BackBufferHeight = SCREEN_HEIGHT;		// ゲーム画面サイズ高さ
+	d3dpp.BackBufferFormat = d3ddm.Format;		// バックバッファの形式
+	d3dpp.BackBufferCount = 1;					//
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;	//
+	d3dpp.EnableAutoDepthStencil = TRUE;		//
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;	//
+	d3dpp.Windowed = bWindow;					//
+	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;	//
+	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;	//
 
-	//Direct3Dデバイスの生成(描画処理と頂点処理をハードウェアで行う)
+	// Direct3Dデバイスの生成(描画処理と頂点処理をハードウェアで行う)
 	if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
 		D3DDEVTYPE_HAL,
 		hWnd,
@@ -249,7 +252,7 @@ HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 		&d3dpp,
 		&g_pD3DDevice)))
 	{
-		//Direct3Dデバイスの生成(描画処理をハードウェア,頂点処理はCPUで行う)
+		// Direct3Dデバイスの生成(描画処理をハードウェア,頂点処理はCPUで行う)
 		if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
 			D3DDEVTYPE_HAL,
 			hWnd,
@@ -257,7 +260,7 @@ HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 			&d3dpp,
 			&g_pD3DDevice)))
 		{
-			//Direct3Dデバイスの生成(描画処理と頂点処理をCPUで行う)
+			// Direct3Dデバイスの生成(描画処理と頂点処理をCPUで行う)
 			if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
 				D3DDEVTYPE_REF,
 				hWnd,
@@ -270,36 +273,36 @@ HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 		}
 	}
 
-	//レンダーステートの設定
-	g_pD3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);//カリングの設定
-	g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE,TRUE);//プレイヤーの中に透明度を加える
+	// レンダーステートの設定
+	g_pD3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);			// カリングの設定
+	g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE,TRUE);			// プレイヤーの中に透明度を加える
 	g_pD3DDevice->SetRenderState(D3DRS_SRCBLEND,D3DBLEND_SRCALPHA);
 	g_pD3DDevice->SetRenderState(D3DRS_DESTBLEND,D3DBLEND_INVSRCALPHA);
 
-	//サンブラーステートの設定
+	// サンブラーステートの設定
 	g_pD3DDevice->SetSamplerState(0,D3DSAMP_MINFILTER,D3DTEXF_LINEAR);
 	g_pD3DDevice->SetSamplerState(0,D3DSAMP_MAGFILTER,D3DTEXF_LINEAR);
 	g_pD3DDevice->SetSamplerState(0,D3DSAMP_ADDRESSU,D3DTADDRESS_WRAP);
 	g_pD3DDevice->SetSamplerState(0,D3DSAMP_ADDRESSV,D3DTADDRESS_WRAP);
 
-	//テクスチャステージステートの設定
+	// テクスチャステージステートの設定
 	g_pD3DDevice->SetTextureStageState(0,D3DTSS_ALPHAOP,D3DTOP_MODULATE);
 	g_pD3DDevice->SetTextureStageState(0,D3DTSS_ALPHAARG1,D3DTA_TEXTURE);
 	g_pD3DDevice->SetTextureStageState(0,D3DTSS_ALPHAARG2,D3DTA_CURRENT);
 
-	//ジョイパッドの初期化処理
+	// ジョイパッドの初期化処理
 	if (FAILED(InitJoypad()))
 	{
 		return E_FAIL;
 	}
 
-	//マウス所得処理
+	// マウス所得処理
 	if (FAILED(InitMouse(hInstance, hWnd)))
 	{
 		return E_FAIL;
 	}
 
-	//キーボード所得処理
+	// キーボード所得処理
 	if (FAILED(InitKeyboard(hInstance, hWnd)))
 	{
 		return E_FAIL;
@@ -307,10 +310,10 @@ HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 
 	
 
-	//モードの設定
+	// モードの設定
 	SetMode(g_mode);
 
-	//デバッグ用表示フォントの生成
+	// デバッグ用表示フォントの生成
 	D3DXCreateFont(g_pD3DDevice, 18, 0, 0, 0,
 		FALSE, SHIFTJIS_CHARSET,
 		OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH,
@@ -319,9 +322,9 @@ HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
 	return S_OK;
 }
 
-//==========================================
-//終了処理
-//==========================================
+//***********************************************
+// 終了処理
+//***********************************************
 void Uninit(void)
 {
 	UninitKeyboard();	// キーボードの終了処理
@@ -347,9 +350,9 @@ void Uninit(void)
 	}
 }
 
-//==========================================
+//***********************************************
 // 更新処理
-//==========================================
+//***********************************************
 void Updata(void)
 {
 	UpdataKeyboard();	// キー入力の更新処理
@@ -406,19 +409,19 @@ void Updata(void)
 #endif
 }
 
-//==========================================
-//描画処理
-//==========================================
+//***********************************************
+// 描画処理
+//***********************************************
 void Draw(void)
 {
-	//画面クリア(バッファバッファ&のクリア）
+	// 画面クリア(バッファバッファ&のクリア）
 	g_pD3DDevice->Clear(0, NULL,
 		(D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER),
 		 D3DCOLOR_RGBA(0, 0, 0, 0), 1.0f, 0);
 
-	//描画開始
+	// 描画開始
 	if (SUCCEEDED(g_pD3DDevice->BeginScene()))
-	{//描画開始が成功した場合
+	{// 描画開始が成功した場合
 
 		// 今の状態の描画
 		switch (g_mode)
@@ -436,7 +439,7 @@ void Draw(void)
 			break;
 		}
 
-#ifdef _DEBUG //デバッグビルド時だけ表示
+#ifdef _DEBUG // デバッグビルド時だけ表示
 
 		DrawFPS();			// FPS表示
 		DrawCamera();		// カメラ
@@ -447,11 +450,11 @@ void Draw(void)
 
 #endif
 
-		//描画終了
+		// 描画終了
 		g_pD3DDevice->EndScene();
 	}
 
-	//バックバッファとフロントバッファの入れ替え
+	// バックバッファとフロントバッファの入れ替え
 	g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
 }
 
@@ -463,15 +466,15 @@ void SetMode(MODE mode)
 	// 今の状態を終了させる
 	switch (g_mode)
 	{
-	case MODE_TITLE:	//タイトル画面
+	case MODE_TITLE:	// タイトル画面
 		UninitTitle();
 		break;
 
-	case MODE_GAME:		//ゲーム画面
+	case MODE_GAME:		// ゲーム画面
 		UninitGame();
 		break;
 
-	case MODE_RESULT:	//リザルト画面
+	case MODE_RESULT:	// リザルト画面
 		UninitResult();
 		break;
 	}
@@ -479,15 +482,15 @@ void SetMode(MODE mode)
 	// 次のモードに初期化
 	switch (mode)
 	{
-	case MODE_TITLE:	//タイトル画面
-		InitGame();
-		break;
-
-	case MODE_GAME:		//ゲーム画面
-		InitGame();
-		break;
-
-	case MODE_RESULT:	//リザルト画面
+	case MODE_TITLE:	// タイトル画面
+		InitGame();		   
+		break;			   
+						   
+	case MODE_GAME:		// ゲーム画面
+		InitGame();		   
+		break;			   
+						   
+	case MODE_RESULT:	// リザルト画面
 		InitResult();
 		break;
 	}
@@ -511,9 +514,9 @@ void DrawFPS(void)
 {
 	RECT rect = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
 	char aStr[256];
-	//文字に代入
+	// 文字に代入
 	wsprintf(&aStr[0], "FPS:%d\n", g_nCountFPS);
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect, DT_LEFT, D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	g_line += 15;
@@ -530,10 +533,10 @@ void DrawCamera(void)
 
 	RECT rect = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
 
-	//文字に代入
+	// 文字に代入
 	sprintf(&aStr[0], "g_camera.rot.x:%3fy:%3fz:%3f\n", pCamera->rot.x, pCamera->rot.y, pCamera->rot.z);
 
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect, DT_LEFT, D3DCOLOR_RGBA(255, 0, 0, 255));
 
 	g_line += 15;
@@ -549,25 +552,25 @@ void DrawDebPlayer(void)
 
 	RECT rect = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
 	char aStr[256];
-	//文字に代入
+	// 文字に代入
 	sprintf(&aStr[0], "g_player.rot.y:%6f\n", pPlayer->rot.y);
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect, DT_LEFT, D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	g_line += 15;
 
 	RECT rect2 = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
-	//文字に代入
+	// 文字に代入
 	sprintf(&aStr[0], "g_player.rotDest.y:%6f\n", pPlayer->rotDest.y);
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect2, DT_LEFT, D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	g_line += 15;
 
 	RECT rect3 = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
-	//文字に代入
+	// 文字に代入
 	sprintf(&aStr[0], "g_player.move:x%6f:y%6f;z%6f\n", pPlayer->move.x, pPlayer->move.y, pPlayer->move.z);
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect3, DT_LEFT, D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	g_line += 15;
@@ -583,9 +586,9 @@ void DrawDCamera(void)
 
 	RECT rect = { 0,g_line,SCREEN_WIDTH,SCREEN_HEIGHT };
 	char aStr[256];
-	//文字に代入
+	// 文字に代入
 	sprintf(&aStr[0], "g_light.Direction.x:%3fy:%3fz:%3f\n", pLight->Direction.x, pLight->Direction.y, pLight->Direction.z);
-	//テキストの描画
+	// テキストの描画
 	g_pFont->DrawText(NULL, &aStr[0], -1, &rect, DT_LEFT, D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	g_line += 15;
